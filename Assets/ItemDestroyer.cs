ca8719f514cb182ea8d44991c49e5704846a9c1a
Felix.Kong <ftmphx@gmail.com>
2018-07-12 00:29:06 +0900

    StepAsideUnityChan 初コミット
    UnityChanのアセットを使って作った3Dランゲームです。

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+    private GameObject camera;
+
+    // Use this for initialization
+    void Start () {
+        this.camera = GameObject.Find("Main Camera");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.camera.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }            
+	}
+}
